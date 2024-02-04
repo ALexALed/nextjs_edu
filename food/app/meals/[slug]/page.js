ef125ea7487ac922e589478bc25c5771c6bf8230
alexaled <alexaled@gmail.com>
2024-02-04 12:45:14 +0100

    detail page

--- food/app/meals/[slug]/page.js
+++ food/app/meals/[slug]/page.js
@@ -1,3 +1,29 @@
-export default function MealDetails({params}) {
-  return <h1>Meals Details { params.slug }</h1>
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+import Image from 'next/image';
+
+export default function MealDetails({ params }) {
+  
+  
+  const meal = getMeal(params.slug);
+  
+  return <>
+    <header className={classes.header}>
+      <div className={classes.image}>
+        <Image fill />
+      </div>
+      <div className={classes.headerText}>
+        <h1>{meal.title}</h1>
+        <p className={classes.creator}>
+          by <a href={`mailto:${'EMAIL'}`}>Name</a>
+        </p>
+        <p className={classes.summary}>Summary</p>
+      </div>
+    </header>
+    <main>
+      <p className={classes.instructions} dangerouslySetInnerHTML={{
+        __html: '...'
+      }}></p>
+    </main>
+  </>
 }
--- food/lib/meals.js
+++ food/lib/meals.js
@@ -1,9 +1,19 @@
 import path from 'path';
 import Database from 'better-sqlite3';
 
-const db = new Database(path.resolve(__dirname, './../../../../meals.db'), { fileMustExist: true })
+
+let db
+
+if (!db) {
+  db = new Database(path.resolve(__dirname, './../../../../meals.db'), { fileMustExist: true })
+}
 
 export async function getMeals() {
-  await new Promise(resolve => setTimeout(resolve, 2000))
+  await new Promise(resolve => setTimeout(resolve, 5000))
   return db.prepare('SELECT * FROM meals').all()
+}
+
+
+export function getMeal(slug) {
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug)
 }
